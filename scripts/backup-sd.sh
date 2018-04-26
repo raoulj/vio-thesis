91dd38cb0f10e1ed57b2276d9053e548a9cf0aed
Raoul Rodriguez <raouljrodriguez@gmail.com>
2018-04-26 19:29:06 -0400

    updated backup to get the destination dir correctly

--- scripts/backup-sd.sh
+++ scripts/backup-sd.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 BACKUP_BASE_PATH="/Users/raoulrodriguez/Developer/vio-thesis/images"
 BACKUP_CHUNK_SIZE="64m"
 echo "Consider the following drives:"
@@ -11,4 +11,4 @@ read disk_number
 echo "What name do you want for the backup?  Enter the full name (followed by .img) and press ENTER:"
 read file_name
 
-sudo dd if="/dev/$disk_number" of="$BACKUP_BASE_PATH/$file_name" bs="$BACKUP_CHUNK_SIZE"
+sudo dd if="/dev/disk$disk_number" of="$BACKUP_BASE_PATH/$file_name" bs="$BACKUP_CHUNK_SIZE"
